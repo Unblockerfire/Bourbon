b4698ffc1d502d3bd3a5e44eb76866dd616a0496
Carson Livezey <carsonlivezey2011@icloud.com>
2026-08-09 21:16:49 -0600

    Add Discord support links

--- BourbonWebsite/components/SiteNav.tsx
+++ BourbonWebsite/components/SiteNav.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Github, HeartHandshake, ShieldCheck } from "lucide-react";
+import { Github, HeartHandshake, MessageCircle, ShieldCheck } from "lucide-react";
 
 import { siteConfig } from "@/lib/siteConfig";
 import { DownloadButton } from "./DownloadButton";
@@ -27,7 +27,7 @@ export function SiteNav() {
           <Github aria-hidden="true" size={20} />
         </a>
       </nav>
-      <div className="navActions">
+      <div className="navActions">`n        <Link className="iconLink" href="/discord" aria-label="Join Bourbon on Discord">`n          <MessageCircle aria-hidden="true" size={19} />`n        </Link>
         <Link
           className="iconLink"
           href="/acknowledgments"
